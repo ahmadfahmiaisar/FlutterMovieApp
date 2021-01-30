31abfe01b82c1ce4a2b18064777836f72c544574
Ahmad Fahmi Aisar <fahmiaisar99@gmail.com>
2021-01-31 03:01:16 +0700

    fix signup with new version firebase flutter

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -3,11 +3,10 @@ part of 'services.dart';
 class AuthServices {
   static FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  static Future<SignInSignUpResult> signUp(String email, String password, String name,
-      List<String> selectedGenres, String selectedLanguage) async {
+  static Future<SignInSignUpResult> signUp(String email, String password,
+      String name, List<String> selectedGenres, String selectedLanguage) async {
     try {
-      UserCredential result = await _firebaseAuth
-          .createUserWithEmailAndPassword(email: email, password: password);
+      UserCredential result = await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
 
       UserModel user = result.user.convertToUser(
           name: name,
--- lib/services/user_services.dart
+++ lib/services/user_services.dart
@@ -5,12 +5,6 @@ class UserServices {
       FirebaseFirestore.instance.collection('users');
 
   static Future<void> updateUser(UserModel userModel) async {
-    String genres = "";
-
-    for(var genre in userModel.selectedGenres){
-      genres += genre + ((genre != userModel.selectedGenres.last) ? ',' : '');
-    }
-
     _userCollection.doc(userModel.id).set({
       'email' : userModel.email,
       'name' : userModel.name,
